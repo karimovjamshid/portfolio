9f932dfbb7ee732b27a2db2c06c49e3b17443e60
karimovjamshid <67834450+karimovjamshid@users.noreply.github.com>
2020-07-25 15:41:37 +0500

    Version 1.0

---
+++ precache-manifest.71daea77d491661bc53cf79ca6048865.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37823355e07097da47031bbcc8facee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b265485a3b72676c8819",
+    "url": "/static/css/main.58f65b5a.chunk.css"
+  },
+  {
+    "revision": "c6cc40074c8510a694a9",
+    "url": "/static/js/2.474276ee.chunk.js"
+  },
+  {
+    "revision": "468f7e79b732f9b80ec5b806f181f6cf",
+    "url": "/static/js/2.474276ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b265485a3b72676c8819",
+    "url": "/static/js/main.cd59e524.chunk.js"
+  },
+  {
+    "revision": "5871c288311b11834f91",
+    "url": "/static/js/runtime-main.bc053696.js"
+  },
+  {
+    "revision": "184f0ab623f8407249c5bce579e273cc",
+    "url": "/static/media/Rhesmanisa.184f0ab6.otf"
+  },
+  {
+    "revision": "2b88f4c10adfc9fe670e7d497d573b73",
+    "url": "/static/media/ava-4.2b88f4c1.png"
+  },
+  {
+    "revision": "0f26dbd6dd0e69126724fc4dca95f1ca",
+    "url": "/static/media/delivery.0f26dbd6.png"
+  },
+  {
+    "revision": "f738a00f403db165c5d81057d6aa38da",
+    "url": "/static/media/dog.f738a00f.svg"
+  },
+  {
+    "revision": "d5f4b911a0407d705ecab505fd9795ce",
+    "url": "/static/media/left-arrow.d5f4b911.svg"
+  },
+  {
+    "revision": "6a6f342ec00c1f8f8a1c33a88ee215a4",
+    "url": "/static/media/right-arrow.6a6f342e.svg"
+  },
+  {
+    "revision": "039e6c56f33e112df0367d1d04246f6d",
+    "url": "/static/media/tajam.039e6c56.png"
+  }
+]);
